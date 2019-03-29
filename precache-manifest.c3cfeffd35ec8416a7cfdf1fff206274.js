1a0e4c011dbff022f754ae7088351e3fe4ead0fd
jose sanchez <sanchez24jose@gmail.com>
2019-03-29 07:35:44 -0500

    Updates

---
+++ precache-manifest.c3cfeffd35ec8416a7cfdf1fff206274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "41e2cc4cd9bbfb0061af",
+    "url": "/myapp/static/js/main.9927cb32.chunk.js"
+  },
+  {
+    "revision": "4cb46573232e297ca2ba",
+    "url": "/myapp/static/js/2.6c8c230b.chunk.js"
+  },
+  {
+    "revision": "41e2cc4cd9bbfb0061af",
+    "url": "/myapp/static/css/main.173018dc.chunk.css"
+  },
+  {
+    "revision": "afec3735a7651a2064b4d5cd0edd0890",
+    "url": "/myapp/index.html"
+  }
+];
